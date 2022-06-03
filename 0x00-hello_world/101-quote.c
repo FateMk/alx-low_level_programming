6694bf641c09def211ede851412596eaf763d3a2
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-03 04:27:33 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
  *main - main block
@@ -7,11 +8,11 @@
 int main (void)
 {
 	
-	 putchar('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+	  int text =strlen("and that piece of art is useful' - Dora Korpar, 2015-10-19\n");
 
-	/*write(2,
-	      'and that piece of art is useful" - Dora Korpar, 2015-10-19\n', 
-	      text);*/
+	write(2,
+	      "and that piece of art is useful' - Dora Korpar, 2015-10-19\n", 
+	      text);
 	return (1);
 }
 
