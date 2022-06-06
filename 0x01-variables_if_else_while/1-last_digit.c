2d94d630f0df1d507baf5167827c6f3ac1e022a6
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-06 11:18:49 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,16 +5,17 @@
  /*Description: prints a random number*/
  /*print if thelast digit of the radom number is zero greater or less then 5*/
  /*Return: 0*/
-int main(void) /* start of the program*/
+/*main function: start of the program*/
+int main(void) 
 {
 	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (n > 5)
+	if (m > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, m);
-	else if (n == 0)
+	else if (m == 0)
 		printf("Last digit of %i is %i and is 0\n", n, m);
 	else
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
