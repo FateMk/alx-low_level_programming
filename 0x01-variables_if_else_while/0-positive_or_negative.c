e5d98d9bf7c5fed1db88666eb6b80fd8f96b8730
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-06 03:24:47 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*main -main block*/
-/*Description: Get a rendom number and print the number*/
+/*main - main block*/
+/*Description: Get a rendom number and print the number and if it is positive, negative, or zero*/
 /*Return: 0*/
 int main(void)
 {
