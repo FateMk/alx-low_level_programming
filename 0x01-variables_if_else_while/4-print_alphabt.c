35ced6e7a88a04b73bd0f9ad66532f774915006b
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-06 13:00:52 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * main -main block
+ * main - main block
  * Decription: print alphabets except e and q
  * Return: 0
  */
@@ -15,5 +15,5 @@ int main(void)
 			putchar(a);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
