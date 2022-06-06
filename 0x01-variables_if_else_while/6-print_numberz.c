e5b1054c71bc85de0bd61ceb42423375816cfcad
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-07 01:56:32 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - main block
-* Description: Print all numbers of base 10, starting from 0.
-* Return: 0
-*/
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ */
 int main(void)
 {
 	int i;
