3bfce87e652daaa2876e2e2ff5dec0d3b2df53db
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-07 01:49:11 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			c = j / 10;
 			d = j % 10;
-			if(a < c || (a == c && b < d))
+			if (a < c || (a == c && b < d))
 			{
 				 putchar(a + '0');
 				 putchar(b + '0');
-				 putchar(32); 
+				putchar(32); 
 				 putchar(c + '0');
 				 putchar(d + '0');
 				 if (!(a == 9 && b == 8))
