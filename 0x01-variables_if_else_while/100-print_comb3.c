e0c3487b39ead149e068ec30b1980018a543d9a6
FateMk <markmurimi151@gmail.com>
2022-06-06 13:00:36 -0700

    possible combinations of tw digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:print combination of 2 digits.
+ * print in ascending order with two digits
+ * can only use putchar function.
+ * use putchar five times maximum
+ * not allowed to use any variable of type char.
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 100; i++)
+	{
+		j = i / 10;
+		k = i % 10;
+
+		putchar(j + '0');
+		putchar(k + '0');
+	}
+	if (i < 99)
+	{
+		putchar(44);
+		puchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
