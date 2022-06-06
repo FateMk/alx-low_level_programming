a87ff2c216ddb946afb702ca58d51d98fa7f82dd
FateMk <markmurimi151@gmail.com>
2022-06-06 15:02:19 -0700

    possible combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print combination of 3 digits
+ * only use putchar function
+ * can only use putchar to a maximum of six times
+ * not allowed to usr any variable of date type char
+ * Return : 0
+ */
+int main(void)
+{
+	int i, j, k ,l;
+
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100;/* hundreds */
+		k = (i / 10) % 10;/* tens */
+		l = i % 10; /*singles/ ones*/
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
