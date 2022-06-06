2f6fc0e43f78631110968cd117b9798a788f2508
FateMk <markmurimi151@gmail.com>
2022-06-06 11:09:30 -0700

    Hexdecimal/ Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i +'0');
+	}
+	for (a = 'a'; a <='f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
