bf083c92855581a7f22a72d02a24a3fd42b6f56c
FateMk <markmurimi151@gmail.com>
2022-06-06 03:45:18 -0700

    Alphabets reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - main block
+ * Decription: print alphabets reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
