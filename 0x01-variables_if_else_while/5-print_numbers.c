902673475edcc1b7b53cc4791b8dcf0241c68d6a
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-07 02:38:36 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 /**
  * main - main block.
@@ -16,22 +15,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-=======
-#include <stdio.h>
-/**
- *main - main block
- *Description: Print all numbers of base 10, starting from 0.
- *Return: 0
- */
-int main(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-	return (0);
-}
->>>>>>> 2da5670c8db45a3b9577425e598910d41ad1d68e
