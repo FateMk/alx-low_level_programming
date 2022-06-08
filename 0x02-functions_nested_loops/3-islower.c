8a44325216d322c125015e74298a0ac9e65802b3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-08 05:53:10 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /*
- * _islower - islower block
+ * _islower - _islower block
  * Description: checks if a character is lowercase
  * Return: 1 
  * else Return: 0
  */
 int _islower(int c)
 {
-	if (c > 96 && c <= 122 )
+	if (c > 96 && c <= 122)
 		return (1);
 	else
 		return (0);
