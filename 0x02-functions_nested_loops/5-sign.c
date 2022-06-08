477043b5f0b2ffac7bd2110eb82c3ff1ddfd3a92
FateMk <markmurimi151@gmail.com>
2022-06-07 20:24:58 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - print_sign block
+ * Description: print the sign of a number
+ * Return: 1 if greater then zero
+ * Return: 0 if it zero else Return: -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchat('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		return(-1);
+
+	}
+}
