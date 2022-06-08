9e4aa1dded4bdb55b85d1c2eb4980047d37ce0bd
FateMk <markmurimi151@gmail.com>
2022-06-07 18:47:34 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main  block
+ * Description: prints the alphabetsin lowercase
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+}
+int main()
+{
+	print_alphabet();
+	getchar();
+	return (0);
+}
