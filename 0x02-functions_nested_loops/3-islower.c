16144f911fc9481743ee4d0467b06a08e5dd02de
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-08 06:10:13 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - _islower block
- * Description: checks if a character is lowercase
+ * Description: checks if a character 'c' is lowercase
  * Return: 1 else Return: 0
  */
 int _islower(int c)
