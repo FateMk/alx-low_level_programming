17a6d034379089e3e5c9b03a3eb12b6cfcb837a5
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-09 17:52:37 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,23 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-	printf("%d largest number is:", largest);
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-	printf("%d largest number is:", largest);
-    }
-    else
-    {
-        largest = c;
-	printf("%d largest number is:", largest);
-    }
-
-    return (largest);
+	int largest;
+	
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	
+	return (largest);
 }
