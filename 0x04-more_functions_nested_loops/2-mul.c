9955612cb30b3c490f6e7e0e10afe26af66046c0
FateMk <markmurimi151@gmail.com>
2022-06-09 15:18:57 -0700

    multiple two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - mul block
+ * @a: int type integer
+ * @b: int type integer
+ * Description: multiplies two integer
+ */
+int mul(int a, int b)
+{
+	int c;
+	/* multiple two integers*/
+	c = a * b;
+	printf("%d\n", c);
+}
