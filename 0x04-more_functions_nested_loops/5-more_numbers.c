45140568e4be58da42221b4db9e31031c9243db3
FateMk <markmurimi151@gmail.com>
2022-06-10 07:49:43 +0300

    print 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - more_number block
+ * Description: prints numbers from 0 to 14
+ * 10 times followed by a new line
+ */
+void more_number(void)
+{
+	int i, j;
+	
+	i = 1;
+	while (i <= 10)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+			       _putchar((j / 10) + '0');
+			       _putchar((j % 10) + '0');
+			}
+			else
+			{
+				_putchar((j % 10) + '0');
+			}
+		}
+		printf('\n');
+		i++;
+	}
+}
