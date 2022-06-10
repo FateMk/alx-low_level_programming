6f2444f8ab6820a7f4c4a1b54576ced479fd5805
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-10 10:17:49 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,10 +15,8 @@ void print_diagonal(int n)
 		for (l = 0; l < n; l++)
 		{
 			for (s = 0; s < l; s++)
-			{
 				_putchar(' ');
-				_putchar('\\');
-			}
+			_putchar('\\');
 			if (l == n - 1)
 				continue;
 			_putchar('\n');
