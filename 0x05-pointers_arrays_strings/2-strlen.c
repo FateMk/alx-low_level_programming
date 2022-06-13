b086f5c1f36e5cb2bb21dbc183b343dffe6e1d4f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-13 16:32:26 +0300

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,12 @@
  * _strlen - _strlen block
  * @s: pointer type integer
  * Description: returns length of string
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int c;
-
+/* for loop to the end of te string*/
 	for (c = 0; s[c] != '\0'; c++);
 	return (c);
 }
