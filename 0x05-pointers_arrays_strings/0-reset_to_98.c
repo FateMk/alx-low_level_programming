93c67da7edd8c7043fd3d3479f0f379eafeefabf
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-13 15:47:04 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 /**
  * reset_to_98 - reset_to_98 block
  * @n: pointer type integer
- * Description: takes a pointer to an int as parameter 
- * and updates the value it poits to to 98
+ * Description: takes a pointer to an int as parameter and 
+ * updates the value it poits to to 98
  */
 void reset_to_98(int *n)
 {
