ae946251a9411800fa85e3ce8872f892c6d15b58
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-14 07:03:39 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	/* for loop to upto length of a string*/
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	n = i  / 2;
+	n = (i - 1) / 2;
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
