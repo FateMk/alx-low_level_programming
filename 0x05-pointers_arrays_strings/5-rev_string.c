0bf8bd6f07026cc837330e30f986a3241747b2f5
FateMk <markmurimi151@gmail.com>
2022-06-14 05:11:38 +0300

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - rev_string block
+ * @s: pointer type string
+ * Decriptions: reverses a string
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char rev;
+	/* length of the string*/
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	j = i;
+	for (i--; k = 0; c < j/2; i--)
+	{
+		rev = s[c];
+		s[c] = s[i];
+		s[i] = rev;
+	}
+}
+
