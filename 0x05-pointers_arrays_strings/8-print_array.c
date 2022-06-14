554ad388ca19548d3ed0f0d56100f7d77c864209
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-14 07:51:08 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,13 @@ void print_array(int *a, int n)
 {
 	int i;
 	/*looping through the array*/
-	for (i = 0; i < n; i++)
+	for (n --, i = 0; i < n; n--, i++)
 	{
 		printf("%d", a[i]);
 		if (n > 0)
 		{
 			printf(", ");
 		}
-		n--;
 	}
 	printf("\n");
 }
