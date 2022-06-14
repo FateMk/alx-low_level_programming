0504e041d7d8ee091d17df851fe39365fc7db7a3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-14 05:20:30 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,8 +2,7 @@
 /**
  * print_rev - print_rev block
  * @s: pointer type string
- * Description: prints a string in reverse 
- * follwed by a new line
+ * Description: prints a string in reverse follwed by a new line
  */
 void print_rev(char *s)
 {
@@ -11,7 +10,7 @@ void print_rev(char *s)
 	/* loops through the string an outputs its length*/
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	}/*Reversing the string*/
+	}  /*Reversing the string*/
 	for (i--; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
