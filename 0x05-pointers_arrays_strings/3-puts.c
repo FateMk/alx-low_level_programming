fd9fbadca1591c826f5da5e16374840b7e80c058
FateMk <markmurimi151@gmail.com>
2022-06-14 05:46:07 +0300

    printing string to standard out puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - _puts block
+ * @str: pointer type string
+ * Description: prints a string to a stdout followed by a new line
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - puts2 block
+ * @str: pointer type string
+ * Description: prints every string character starting
+ * with the first character followed by a new line
+ */
+void puts(char *str)
+{
+	int i , j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(i % 2 == 0)
+			j += str[i];
+		_putchar(j);
+	}
+	_putchar('\n');
+}
