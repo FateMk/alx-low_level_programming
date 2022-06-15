ae0247a95a70a682b95ffa11cc1e306820a5d0b3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-16 00:58:21 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
  * _strncat - _strncat block
- * @dest: pointer type string
- * @src: pointer type string
- * @n: int type integer
- * Description: concatenate two strings
- * will use at most n bytes from src and src does not
- * need to be null-terminated i contains n  or more bytes
- * Return: dest
+ * @dest: pointer type string.
+ * @src: pointer type string.
+ * @n: int type integer.
+ * Description: concatenate two strings will use at most n bytes from src.
+ * src does not need to be null-terminated i contains n  or more bytes.
+ * Return: dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	/* looping both through dest and src*/
-	for (i = 0; dest[i] != '\0'; ++i)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
 	for (j = 0; j < n && src[j] != '\0'; j++, i++)
