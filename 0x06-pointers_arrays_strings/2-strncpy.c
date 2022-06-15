de6a334a3483041818db5c6c92b4ac82b986e7e3
FateMk <markmurimi151@gmail.com>
2022-06-16 01:02:21 +0300

    coping a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - _strncpy block
+ * @dest: pointer type string
+ * @src: pointer type string
+ * @n: int type integer
+ * Description: copies a string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	/* for loop */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; n > i; i++)
+	{
+		dest[i] = '\0';
+        }
+	return (dest);
+}
