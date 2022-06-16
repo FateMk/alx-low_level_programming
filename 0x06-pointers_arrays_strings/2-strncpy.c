33856702be8978ccf5f5ba42447cb8cd00d9bfb4
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-16 20:40:16 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	
-	while (i != n)
+	for (; n > i; i++)
 	{
 		dest[i] = '\0';
-		i++;
         }
 	return (dest);
 }
