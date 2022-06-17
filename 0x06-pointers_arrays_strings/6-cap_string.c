45ad246146b0d3f172dce39006a3ec5cead0f958
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-17 16:55:45 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,15 +15,14 @@ char *cap_string(char *s)
 	
 	for (i = 0; s[i] != '\0'; i++)
 		;
-	
 	for (j = 0; j < i; j++)
 	{
 		if (count == 0)
 		{
 			if (s[j] >= 'a' && s[j] <= 'z')
 				s[j] = s[j] - 32;
-		else
-			count++;
+			else
+				count++;
 		}
 		if (s[j] == '!' || s[j] == '"' || s[j] == '(' || s[j] == ')' || s[j] == ',' || s[j] == '.' || s[j] == '{' || s[j] == '}' || s[j] == ';' || s[j] == '?' || s[j] == '\n' || s[j] == '\t' || s[j] == ' ')
 			count = 0;
