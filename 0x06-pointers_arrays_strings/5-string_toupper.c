e135ccbecd51fc3c039cfa8c899f29bc699034a2
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-17 16:00:47 +0300

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,16 +3,18 @@
  * string_toupper - function returning apointer to a char 
  * @str: pointer type character
  * Description: changes all lowercase letter to uppercase
+ * Return: str
  */
 char *string_toupper(char *str)
 {
 	int i;
 	/*looping through thr string*/
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{	
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
 	}
+	return (str);
 }
