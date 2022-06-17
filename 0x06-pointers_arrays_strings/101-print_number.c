c1092e16bc7bfd832e6e4b8e9f6cd57f852d53c6
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-17 18:20:27 +0300

    Add files via upload

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - print_number block
+ * @n: int type integer
+ * Description: prints an integer.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if((n / 10) > 0)
+	{
+		print_number(n / 10);
+	}
+		putchar((n % 10) + '0');
+}
