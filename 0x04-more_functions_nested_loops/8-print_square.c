5b14503062e03fa8193c550c26961bfff3c97e69
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-18 19:42:01 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,8 +2,8 @@
 /*
  * print_square - print_square block
  * @size: int type integer
- * Description: prints size of a square using character #
- * move to a new line if size is 0 or less
+ * Description: use  char # to print.
+ * If size is 0 or less, the function should print only a new line
  */
 void print_square(int size)
 {
