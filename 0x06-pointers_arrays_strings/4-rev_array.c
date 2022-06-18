b411d94f3401b9ba5b00119412a4214ec8d100ae
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-18 05:25:37 +0300

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,16 +2,14 @@
 /**
  * reserve_array - reverse_array block
  * @a: pointer type integer
- * @n: int type integer
+ * @n: number of elements of the array 
  * Description: reverses array of integers 
- * n is the number of elements of the array 
  */
 void reverse_array(int *a, int n)
 {
 	int i, rev;
-	/* reversing a sting by swapping*/
 	n--;
-	for (i = 0; i < n; i++, n--);
+	for (i = 0; i < n; i++, n--)
 	{
 		rev = a[i];
 		a[i] = a[n];
