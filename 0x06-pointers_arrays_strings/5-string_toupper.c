114760e7911efa94e6b0404b747d87fd84140572
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-18 05:36:46 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,13 @@
 */
 void reverse_array(int *a, int n)
 {
-int i, temp;
-n--;
-for (i = 0; i < n; i++, n--)
-{
-temp = a[i];
-a[i] = a[n];
-a[n] = temp;
-}
+  int i;
+	int h;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
 }
