2966addfbcc08c2b3f7b65e5dbfc08fb84bc677d
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-18 05:26:41 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * reserve_array - reverse_array block
- * @a: pointer type integer
- * @n: number of elements of the array 
- * Description: reverses array of integers 
+ * reserve_array - reverse array block.
+ * @a: pointer type integer.
+ * @n: number of elements of the array. 
+ * Description: reverses array of integers. 
  */
 void reverse_array(int *a, int n)
 {
 	int i, rev;
+	
 	n--;
 	for (i = 0; i < n; i++, n--)
 	{
