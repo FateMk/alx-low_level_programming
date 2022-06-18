921710f1f5b61008f10c847ba2aad439e9b20f09
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-18 05:59:12 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
  * rot13 - rot13 block
  * @s: pointer type character.
  * Description: encode a string using rot13.
  * Return: s.
  */
-
 char *rot13(char *s)
 {
 	int i, j;
-	
 	char az[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char nz[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	
+	/* for  loop to encode*/
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j =0; j < 52; j++)
