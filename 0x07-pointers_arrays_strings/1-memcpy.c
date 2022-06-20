bc081e247a219254bb052521721d964dd23de63f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-21 00:17:17 +0300

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - _memcpy block
+ * @dest: pointer type character
+ * @src: pointer type character
+ * n: number of bytes to copied
+ * Description: copies memory are
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	 i = 0;
+
+	 while (n > 0)
+	 {
+		 dest[i] = src[i];
+		 i++;
+		 n--;
+	 }
+}
