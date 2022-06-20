ecedd4da105201033b228f268d54872eea9ef1c1
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-21 00:34:45 +0300

    Add files via upload

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _strstr - _strstr block
+ * @haystack: pointer type character
+ * @needle: pointer type character
+ * Description: loctas a substring
+ * Return: 
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *p;
+
+	for(haystack[i] != '\0')
+	{
+
