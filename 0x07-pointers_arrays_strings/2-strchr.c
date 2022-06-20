eac79785a531a005c8f3e787435c7ab9c83e5c2c
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-21 00:23:16 +0300

    Add files via upload

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - _strchr block
+ * @s: pointer type string
+ * @c: character
+ * Description: locates a character in a string.
+ * return: null  if charcter is not found
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if(*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+	}
+	return (s+ 1);
+}
