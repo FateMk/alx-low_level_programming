024e923a510c8edeeea1fb196dee2c3927ec1648
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-21 00:30:54 +0300

    Add files via upload

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * _strpbrk - _strpbrk block
+ * @s: pointer type string
+ * @accept: pointer type string
+ * Description: searches a string for any of a set of bytes
+ * Return: pointer in s matching accept, Null if not found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int *ss;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				ss = &s[i];
+				return (ss);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
