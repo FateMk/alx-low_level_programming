3e7a600d648499ea5649a012556880f5cb1b19d9
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-23 08:05:32 +0300

    Dreams feel real while we're in them

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 /**
  * _strlen_recursion - _strlen_recursion block
  * @s: pointer type character
- * Description: length of a string
+ * Return: 0 if s size is null 
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
-		return 0;
+	if (*s == '\0')
+		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
 
