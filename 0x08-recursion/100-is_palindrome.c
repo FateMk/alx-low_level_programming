fe1e4d3119099ff750d327c94a1545d530e93a0c
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-23 15:42:02 +0300

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -45,17 +45,3 @@ int _strlen(char *s)
 	else
 		return (1 + _strlen(s + 1));
 }
-int main(void)
-{
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
-}
