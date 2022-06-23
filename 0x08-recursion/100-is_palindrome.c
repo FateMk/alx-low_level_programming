dc9d684270d7581a7c1193ce2dcd4b6e4adca6f7
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-23 15:37:24 +0300

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+int _strlen(char *s);
+int pali(char *s, int i);
+/**
+ * is_palindrome - is_palidome block
+ * @s:pointer type integer
+ * Return: 1 if a string is a palindrome else 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int len;
+	len = _strlen(s) - 1;
+	
+	return (pali(s, --len));
+}
+/**
+ * pali - recursive check of palindrome
+ * @s: string
+ * @i: length of string
+ * Return: 1 if pa;indrome, 0 if not
+ */
+int pali(char *s, int i)
+{
+	if (*s == *(s + i))
+	{
+		if(i <= 0)
+			return (1);
+		else
+			return (pali(s + 1, i - 2));	
+	}
+	else
+	{
+		return 0;
+	}
+}
+/**
+ * _strlen - gets the length of string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (1 + _strlen(s + 1));
+}
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
