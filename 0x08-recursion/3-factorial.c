0ab0395964f8c1c747f2b124da8907735729e6b7
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-23 07:56:23 +0300

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,14 +2,14 @@
 /**
  * factorial - factorial block
  * @n: integer
- * Decription: returns the factorial of agiven number
+ * Decription: -1 if n is less than 0 else  return 1
  */
 int factorial(int n)
 {
 	if (n == 1)
-		return 1;
+		return (1);
 	else if (n < 1)
-		return -1;
-	return n * factorial(n - 1);
+		return (-1);
+	return (n * factorial(n - 1));
 }
 
