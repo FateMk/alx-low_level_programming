9d3ab96960dcc883ef4489a37ace6562ba91e80b
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-23 08:10:58 +0300

    Add files via upload

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * sqrt -  sqrt  block
+ * @a: integer
+ * @b: integer
+ * Description: looks for a square to a number
+ */
+
+int sqrt(int a, int b)
+{
+	if (b * b == a)
+		return b;
+	else if (b * b > a)
+		return -1;
+	return sqrt(a, b + 1);
+}
+
+/**
+ * _sqrt_recursion - _sqrt_recursion block
+ * @n: integer
+ * Returns: -1 if n doesn't have a square root
+ */
+int _sqrt_recursion(int n)
+{
+	return sqrt2(n, 1);
+}
