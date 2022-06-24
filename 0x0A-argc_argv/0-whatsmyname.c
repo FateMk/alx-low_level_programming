4cd40ee5b8e40783155737545e899e08f78bb1cb
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-24 14:37:31 +0300

    Add files via upload

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * @argc: counter 
+ * @argv: a vector array
+ * Description: prints the program name
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
