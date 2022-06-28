fdc75f57e4a603fa98f7d43fe029be9cb7250caf
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-28 17:58:58 +0300

    files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - craete_array block
+ * @size: integer
+ * @c: character
+ * Return: Null if size is equal to 0 or if it fails
+ *        else return a pointer to an array
+ */
+char *craete_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *j;
+
+	if (size == 0)
+		return NULL;
+	j = malloc(size *sizeof(*j));
+	if (j == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		j[i] = c;
+	}
+	return (j);
+}
+
+
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ustdio.h>
+/**
+ * _putchar - _putchar block
+ * @c: character
+ * Return 
+ */
+int _putchar(char c)
+{
+	return (write(1 &c 1));
+}
