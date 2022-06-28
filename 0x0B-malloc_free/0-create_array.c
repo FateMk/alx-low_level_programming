657b9c111c4d9bdd1c4bf5cfac1d37449fcec95f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-06-28 19:46:55 +0300

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,20 +7,18 @@
  * Return: Null if size is equal to 0 or if it fails
  *        else return a pointer to an array
  */
-char *craete_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *j;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	j = malloc(sizeof(char) * size);
 	if (j == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-	{
 		j[i] = c;
-	}
 	return (j);
 }
 
