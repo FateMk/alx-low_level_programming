44216c20e11472947210cfbba48ca4a50666970c
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-05 04:13:19 +0300

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - main block
- * @argc: number of elements
- * @argv: Vector array
  * Return: 0
  */
-int main(int argc, char *argv[]) {
-	printf("program's name is: %s\n",argv[0]);
-	return 0;
+iint main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
 }
