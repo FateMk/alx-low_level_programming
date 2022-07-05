4df7396dbe08c5a8cda2ac155010251a02e5539d
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-05 04:10:44 +0300

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * @argc: number of elements
+ * @argv: Vector array
+ * Return: 0
+ */
+int main(int argc, char *argv[]) {
+	printf("program's name is: %s\n",argv[0]);
+	return 0;
+}
---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,10 @@
+#include <ustdio.h>
+/**
+ * _putchar - _putchar block
+ * @c: character
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return(write(1 &c 1));
+}
