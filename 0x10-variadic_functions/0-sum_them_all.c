a45f833b4662955de3df327b31f17a3543f3bceb
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-09 06:13:12 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
-		sum = va_arg(ptr, int);
+		sum += va_arg(ptr, int);
 	if (n == 0)
 		return (0);
 	va_end(ptr);
