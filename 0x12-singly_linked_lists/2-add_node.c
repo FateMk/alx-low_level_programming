53f9a0fb0743ce5805eef1da75458a98830d51e3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-19 07:25:08 +0300

    linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,56 +11,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    usigned int i;
+	usigned int i;
+	
 	list_t *start = malloc(sizeof(list_t *));
-    if (str = NULL)
-    {
-        start->len = 0;
-        star->next = *head
-    }
-    if (start == NULL)
-    return (NULL);
+	if (str = NULL)
+	{
+		start->len = 0;
+		start->next = *head;
+	}
+	if (start == NULL)
+		return (NULL);
 	start->next = *head;
 	*head = start;
-    star->str = strdup(str);
-    for (i = 0; str[i]; i++)
-    ;
-    star->len = i;
-    return (start);
-}
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node(&head, "Alexandro");
-    add_node(&head, "Asaia");
-    add_node(&head, "Augustin");
-    add_node(&head, "Bennett");
-    add_node(&head, "Bilal");
-    add_node(&head, "Chandler");
-    add_node(&head, "Damian");
-    add_node(&head, "Daniel");
-    add_node(&head, "Dora");
-    add_node(&head, "Electra");
-    add_node(&head, "Gloria");
-    add_node(&head, "Joe");
-    add_node(&head, "John");
-    add_node(&head, "John");
-    add_node(&head, "Josquin");
-    add_node(&head, "Kris");
-    add_node(&head, "Marine");
-    add_node(&head, "Mason");
-    add_node(&head, "Praylin");
-    add_node(&head, "Rick");
-    add_node(&head, "Rick");
-    add_node(&head, "Rona");
-    add_node(&head, "Siphan");
-    add_node(&head, "Sravanthi");
-    add_node(&head, "Steven");
-    add_node(&head, "Tasneem");
-    add_node(&head, "William");
-    add_node(&head, "Zee");
-    print_list(head);
-    return (0);
+	start->str = strdup(str);
+	for (i = 0; str[i]; i++)
+		;
+	start->len = i;
+	return (start);
 }
