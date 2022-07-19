4a55c94b9e6646af4868017095f85bd53981e9be
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 02:40:29 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,25 +7,16 @@
  */
  listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  {
-    int i;
-
-    if (index == NULL)
-    {
-        return (NULL);
-    }
-    i = 0;
-    while (i < index)
-    {
-        if (head)
-        {
-            head = head->next;
-        }
-        i++;
-    }
-    if (head)
-    {
-        return (head);
-    }
-    else{
-        return (NULL);
-    }
+  unsigned int i = 0;
+  
+  if (!head)
+   return (0);
+  while (head)
+  {
+   if (index == i)
+    return (head);
+   i++;
+   head = head->next;
+  }
+  return (0);
+ }
