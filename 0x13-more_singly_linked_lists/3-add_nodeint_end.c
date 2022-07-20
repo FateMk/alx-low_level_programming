20f1a6ff7e6bc74db83f84e69c359a0801395ea6
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 04:10:57 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,26 +7,26 @@
  */
  listint_t *add_nodeint_end(listint_t **head, const int n)
  {
-  listint_t *last, *new_last;
-  
-  new_last = malloc(sizeof(listint_t));
-  if (new_last == NULL)
-  {
-   return (NULL);
-  }
-  new_last->n = n;
-  new_last->next = NULL;
-  last = *head;
-  if (!*head)
-		*head = new_last;
-  else
+	 *last, *new_last;
+	 
+	 new_last = malloc(sizeof(listint_t));
+	 if (new_last == NULL)
 	 {
-   while (last->next != NULL)
-   {
-       last = last->next;
-   }
-   last->next = new_last;
-  }
-  return (new_last);
+		 return (NULL);
+	 }
+	 new_last->n = n;
+	 new_last->next = NULL;
+	 last = *head;
+	 if (!*head)
+		 *head = new_last;
+	 else
+	 {
+		 while (last->next != NULL)
+		 {
+			 last = last->next;
+		 }
+		 last->next = new_last;
+	 }
+	 return (new_last);
  }
  
