da2c3bdbc2e517dabc9a8e6bbac9829300a901d3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 03:52:43 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,13 @@
  */
  size_t print_listint(const listint_t *h)
  {
-    int i = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        i++;
-    }
-    return (i);
+  int i = 0;
+  
+  while (h)
+  {
+      printf("%d\n", h->n);
+      h = h->next;
+      i++;
+  }
+  return (i);
  }
- 
