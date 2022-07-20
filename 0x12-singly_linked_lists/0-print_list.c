f7179d09f5983cc67fc5b4fe0ad08870fb948a5b
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 15:41:36 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <stdio.h>
+#include "lists.h"
 /**
  * print_list - prints all elements of a list_t list.
  * @h: constant list_t
@@ -7,10 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-    unsigned int i = 0;
-	const list_t *temp_h = h;
-
-	for (; temp_h; temp_h = temp_h->next, i++)
-		printf("[%d] %s\n", temp_h->len, temp_h->str);
+	unsigned int i = 0;
+	
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
 	return (i);
 }
