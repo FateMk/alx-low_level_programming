35c1fac31e4f278de67009d9be5c630da96037b6
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 04:20:02 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,12 @@ void free_listint2(listint_t **head)
 {
     listint_t *victim;
 
-    if (head == NULL)
+    if (head)
     {
-        return;
-    }    
-    while (*head != NULL)
-    {
-        victim = *head;
-        *head = (*head)->next;
-        free(victim);
-    }
+        while (*head != NULL)
+        {
+            victim = *head;
+            *head = victim->next;
+            free(victim);
+        }
 }
