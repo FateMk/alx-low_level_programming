b32eb2ea4ebbb445d33b477979ba8952289cb7b3
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 15:48:47 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,23 +7,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i;
+	char *dup;
+	int len;
 	list_t *new;
-	
-	new  = malloc(sizeof(list_t *));
-	if (str == NULL)
-	{
-		new->len = 0;
-		new->next = *head;
-	}
+
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
 	new->next = *head;
+
 	*head = new;
-	new->str = strdup(str);
-	for (i = 0; str[i]; i++)
-		;
-	new->len = i;
+
 	return (new);
 }
-
