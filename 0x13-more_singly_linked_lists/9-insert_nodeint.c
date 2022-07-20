eae607c31c5d1d54a978e4916310537e74d6444d
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 03:45:02 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,37 +1,45 @@
 #include "lists.h"
-/**
- * insert_nodeint_at_index - inserts a new node at agiven position
- * @head: pointer to head of listint_t
- * @idx: index of list were nnew node should be added
- * @n: integer to be added
- * Return: address of the new node or NULL if it failed
- */
- listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
- {
-  int size = 0;
-  listint_t *new_node, *new  = *head;
 
-  new_node = malloc(sizeof(listint_t));
-  if (new_node == NULL)
-  {
-   return (NULL);
-  }
-  new_node->n = n;
-  if (idx == 0)
-  {
-      new_node->next = new;
-      *head = new_node;
-      return (new_node);
-  }
-  while (size < (idx - 1))
-  {
-      if (new == NULL || new->next == NULL)
-      {
-          return (NULL);
-      }
-      new = new->next;
-  }
-  new_node->next = new->next;
-  new->next = new_node;
-  return (new_node);
- }
+/**
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
+}
