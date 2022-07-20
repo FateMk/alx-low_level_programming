78ef4241e721ea6e5f5be795384e467e2f572ac1
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 03:46:22 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,32 +14,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *copy = *head;
-	unsigned int node;
+	listint_t *new_node, *temp_node = *head;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
+	while (temp_node && i < idx - 1)
 	{
-		new->next = copy;
-		*head = new;
-		return (new);
+		temp_node = temp_node->next;
+		i++;
 	}
-
-	for (node = 0; node < (idx - 1); node++)
+	if (temp_node || (i == idx - 1 || idx == 0))
 	{
-		if (copy == NULL || copy->next == NULL)
-			return (NULL);
-
-		copy = copy->next;
+		new_node = malloc(sizeof(listint_t));
+		if (new_node)
+		{
+			new_node->n = n;
+			if (idx == 0)
+			{
+				new_node->next = *head;
+				*head = new_node;
+			}
+			else
+			{
+				new_node->next = temp_node->next;
+				temp_node->next = new_node;
+			}
+			return (new_node);
+		}
 	}
-
-	new->next = copy->next;
-	copy->next = new;
-
-	return (new);
+	return (NULL);
 }
