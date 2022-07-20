b9766dec7aa4dd7623e309dc99d997ee079d0fdf
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-20 04:46:29 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,9 @@
 size_t print_list(const list_t *h)
 {
     unsigned int i = 0;
-    
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        printf("[0] (nil)\n");
-        else
-        printf("[%d] %s\n", h->len, h->str);
-        h = h->next;
-        i++;
-    }
-    return (i);
-}
+	const list_t *temp_h = h;
 
+	for (; temp_h; temp_h = temp_h->next, i++)
+		printf("[%d] %s\n", temp_h->len, temp_h->str);
+	return (i);
+}
