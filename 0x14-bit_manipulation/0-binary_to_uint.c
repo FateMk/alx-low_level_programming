e2c29748ede7d49ef5e3b9e5b5830a1418c74660
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-23 11:03:27 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 int _strlen(const char *s);
 /**
  * binary_to_uint - converts binary number to an unsigned int
@@ -7,25 +8,30 @@ int _strlen(const char *s);
  */
  unsigned int binary_to_uint(const char *b)
  {
-   int i, dcimal = 1, mul = 0;
+  int i, dcimal = 1, mul = 0;
 
-   int len = _strlen(b);
-   for (i = (len - 1) ; i >= 0; i--)
-   {
-    if (b[i] != '0' && b[i] != '1')
-    return (0);
-    if (b[i] == '1')
-    mul += dcimal;
-    dcimal *= 2;
-   }
-   return (mul);
+  int len = _strlen(b);
+  for (i = (len - 1) ; i >= 0; i--)
+  {
+  if (b[i] != '0' && b[i] != '1')
+  {
+   return (0);
+  }
+  if (b[i] == '1')
+  {
+   mul += dcimal;
+   dcimal *= 2;
+  }
+  return (mul);
  }
  int _strlen(const char *s)
  {
   int i;
 
   if (s == 0)
-  return (0);
+  {
+   return (0);
+  }
   while (s[i] != '\0')
   {
     i++;
