4df62d3cfcb1141aaf1b5b3f5d15714df1f9c562
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-23 11:06:48 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,33 +9,27 @@ int _strlen(const char *s);
  unsigned int binary_to_uint(const char *b)
  {
   int i, dcimal = 1, mul = 0;
-
+  
   int len = _strlen(b);
   for (i = (len - 1) ; i >= 0; i--)
   {
-  if (b[i] != '0' && b[i] != '1')
-  {
-   return (0);
+   if (b[i] != '0' && b[i] != '1')
+    return (0);
+   if (b[i] == '1')
+    mul += dcimal;
+    dcimal *= 2;
   }
-  if (b[i] == '1')
-  {
-   mul += dcimal;
-   dcimal *= 2;
-  }
-  return (mul);
+   return (mul);
  }
  int _strlen(const char *s)
  {
   int i;
 
   if (s == 0)
-  {
-   return (0);
-  }
+  return (0);
   while (s[i] != '\0')
   {
     i++;
   }
   return (i);
  }
- 
