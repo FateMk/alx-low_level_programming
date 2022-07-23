146f9236a4725710aec46d8115373cf6e557a705
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-23 11:10:11 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,28 +5,21 @@
  */
  void print_binary(unsigned long int n)
  {
-     //int i = CHAR_BIT * sizeof(n);
-     //while (i--)
-     //{
-      //  putchar('0' + ((n >> i) & 1));
-     //}
-    unsigned long int temp;
-	int shifts;
-
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+	 unsigned long int temp;
+	 int shifts;
+	 
+	 if (n == 0)
+	 {
+		 printf("0");
+		 return;
+	 }
+	 for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
 		;
-
-	for (; shifts >= 0; shifts--)
-	{
-		if ((n >> shifts) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
-}
+	 for (; shifts >= 0; shifts--)
+	 {
+		 if ((n >> shifts) & 1)
+			 printf("1");
+		 else
+			 printf("0");
+	 }
+ }
