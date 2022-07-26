00dcdd86c12bc63518e68475c2c9d4838d34317f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-27 01:37:16 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,38 @@
 #include "main.h"
 /**
- * append_textto_file - appends text at the end of a file 
- * @filename: name of the file to be created
- * @text_content: NULL terminated string to be add
- * Return: 1 on success and -1 on failure
- *         -1 if filename is NULL
- *          if file content is NULL add Nothing
- *          1 if the file exists and -1 if file doesnot
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ * Return: length of string
  */
- int append_text_to_file(const char *filename, char *text_content)
- {
-	 int ptr, w, len = 0;
-	 
-	 if (filename == NULL)
-		 return (-1);
-	 if (text_content != NULL)
-	 {
-		 for (len = 0; text_content[len];)
-			 len++;
-	 }
-	 ptr = open(filename, O_WRONLY | O_APPEND);
-	 w = write(ptr, text_content, len);
-	 if (ptr == -1 || w == -1)
-		 return (-1);
-	 close(ptr);
-	 return (1);
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of file to have text appended to
+ * @text_content: text to be appended to the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (close(fd) == -1 ? -1 : 1);
+	len = _strlen(text_content);
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (close(fd) == -1 ? -1 : 1);
 }
