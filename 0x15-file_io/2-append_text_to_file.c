d3be3bfd30366bc724dc9757011d44474efafef1
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-27 01:35:58 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,24 +10,19 @@
  */
  int append_text_to_file(const char *filename, char *text_content)
  {
-    int ptr, w, len = 0;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	ptr = open(filename, O_WRONLY | O_APPEND);
-	w = write(ptr, text_content, len);
-
-	if (ptr == -1 || w == -1)
-		return (-1);
-
-	close(ptr);
-
-	return (1);
+	 int ptr, w, len = 0;
+	 
+	 if (filename == NULL)
+		 return (-1);
+	 if (text_content != NULL)
+	 {
+		 for (len = 0; text_content[len];)
+			 len++;
+	 }
+	 ptr = open(filename, O_WRONLY | O_APPEND);
+	 w = write(ptr, text_content, len);
+	 if (ptr == -1 || w == -1)
+		 return (-1);
+	 close(ptr);
+	 return (1);
 }
