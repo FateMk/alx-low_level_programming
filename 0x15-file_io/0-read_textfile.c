6fdaff42e5258c6efa44e8592e35edc62e9f6b5f
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-27 01:28:00 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,22 +9,22 @@
  *         0 if write fails and doesn't write the expected amount of bytes
  */
  ssize_t read_textfile(const char *filename, size_t letters)
- { 
-         ssize_t ptr, r, w;
+ {
+	 ssize_t ptr, r, w;
          char *buffer;
          
          if (filename == NULL)
 		 return (0);
          buffer = malloc(sizeof(char) * letters);
          if (buffer == NULL)
-         return (0);
+		 return (0);
          ptr = open(filename, O_RDONLY);
          r = read(ptr, buffer, letters);
          w = write(STDOUT_FILENO, buffer, r);
          if (ptr == -1 || r == -1 || w == -1 || w != r)
          {
-            free(buffer);
-            return (0);
+		 free(buffer);
+		 return (0);
          }
          free(buffer);
          close(ptr);
