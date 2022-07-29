2b905673339f1b868167a3c6bdf3468405f1445c
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-29 22:31:20 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,10 @@
 #include "main.h"
+
 /**
- * create_file - creates a file
- * @filename: name of the file to be created
- * @text_content: NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure 
- *          if file already exist truncate it
- *          if filename is NULL return -1
- *          if text_content is NULL create an empty file
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
  */
  int create_file(const char *filename, char *text_content)
  {
