d6009eb6db80d3a42f997d1dcef1e199da889e50
FateMk <93762003+FateMk@users.noreply.github.com>
2022-07-29 22:32:26 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  int create_file(const char *filename, char *text_content)
  {
 	 int o, w, len = 0;
-	 /* creating a file*/
+	 
 	 if (filename == NULL)
 		 return (-1);
 	 if (text_content != NULL)
