d799c148e52bd0e982c8a273116e287cf6f85031
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-03 03:32:24 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,17 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+    const dlistint_t *tmp;
     size_t count;
-
-    while (h && h->prev)
+    
+    tmp = h;
+    while (tmp && tmp->prev)
     {
-        h = h->prev;
+        tmp = tmp->prev;
     }
-    for (count = 0; h; count++, h = h->next)
+    for (count = 0; tmp; count++, tmp = tmp->next)
     {
-        printf("%d\n", h->n);
+        printf("%d\n", tmp->n);
     }
     return (count);
 }
