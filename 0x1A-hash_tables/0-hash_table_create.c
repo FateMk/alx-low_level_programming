279db5f2674d770834fd7534fc2ecb5b8a935759
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 03:37:28 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-
 	hsh = malloc(sizeof(hash_table_t));
 	if (hsh == NULL)
 		return (NULL);
-
 	hsh->array = malloc(sizeof(hash_node_t *) * size);
 	if (hsh->array == NULL)
 	{
@@ -24,7 +22,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hsh->size = size;
-
 	for (i = 0; i < size; i++)
 		hsh->array[i] = NULL;
 	return (hsh);
