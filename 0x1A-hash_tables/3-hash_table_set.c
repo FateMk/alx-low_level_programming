3a548f657de47679adf33adf3bbf13deb1040ad9
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 03:13:04 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,72 @@
+
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: hash table to add to
- * @key: key to add
- * @value: value to store for key
- * Return: 1 if success, 0 if fail
+ * add_hash_node - adds hash node to hash table array.
+ * @key: the key you want to add to the hash table
+ * @value: the value associated with the key
+ *
+ * Return: new node or NULL on failure
+ */
+hash_node_t *add_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (!new_node->key || !new_node->value)
+	{
+		if (new_node->key)
+			free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	return (new_node);
+}
+
+
+/**
+ * hash_table_set - check the code for a software development program students.
+ * @ht: the hash table to be added to
+ * @key: the key you want to add to the hash table
+ * @value: the value associated with the key
+ *
+ * Return: 1 on success 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newpair, *tmp;
 	unsigned long int idx;
+	hash_node_t *node, *temp, *new_node;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (!ht || !key || !value || !ht->array || strlen(key) == 0)
 		return (0);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[idx];
-
-	if (tmp != NULL)
+	idx = key_index((unsigned char *)key, ht->size);
+	node = temp = ht->array[idx];
+	if (node)
 	{
-		while (tmp)
+		while (temp)
 		{
-			if (strcmp(tmp->key, key) == 0)
+			if (!strcmp(temp->key, key))
 			{
-				tmp->value = strdup(value);
+				free(temp->value);
+				temp->value = strdup(value);
 				return (1);
 			}
-			tmp = tmp->next;
+			temp = temp->next;
 		}
 	}
 
-	newpair = malloc(sizeof(hash_node_t));
-	if (newpair == NULL)
+	new_node = add_hash_node(key, value);
+	if (!new_node)
 		return (0);
-
-	newpair->key = strdup(key);
-	newpair->value = strdup(value);
-	newpair->next = NULL;
-
-	if (ht->array[idx] != NULL)
-		newpair->next = ht->array[idx];
-
-	ht->array[idx] = newpair;
+	new_node->next = node;
+	ht->array[idx] = new_node;
 
 	return (1);
 }
