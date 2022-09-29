56435fa929f85bab26385e0e4fad728b8c132efb
FateMk <93762003+FateMk@users.noreply.github.com>
2022-09-29 03:07:47 +0300

    key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,8 @@
  * key_index - gives you the index of a key
  * @key: key
  * @size: size of array of hash table
- * Return: index where key/value pair should be stored in array of hash table
+ * Return: index where key/value pair should be 
+ *         stored in array of hash table
  */
  unsigned long int key_index(const unsigned char *key, unsigned long int size)
  {
